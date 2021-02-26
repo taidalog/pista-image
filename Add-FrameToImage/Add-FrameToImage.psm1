034f8f40351b9044b4582137ca65eaf938c93193
taidalog <72747279+taidalog@users.noreply.github.com>
2021-02-27 00:00:25 +0900

    add Add-FrameToImage.psm1 This is the first version. It works.

---
+++ Add-FrameToImage/Add-FrameToImage.psm1
@@ -0,0 +1,101 @@
+Add-Type -AssemblyName System.Drawing
+
+function Add-FrameToImage {
+    [CmdletBinding()]
+    param (
+        # Specifies a path to one or more locations.
+        [Parameter(Mandatory=$true,
+                   Position=0,
+                   ParameterSetName="Path",
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   HelpMessage="Path to one or more locations.")]
+        [Alias("PSPath")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({Test-Path $_ -PathType 'Leaf'})]
+        [string[]]
+        $Path,
+
+        [ValidateRange(1,[int]::MaxValue)]
+        [int]
+        $LineWidth = 1,
+
+        [ValidateRange(0,255)]
+        [int]
+        $Alpha = 255,
+        
+        [ValidateRange(0,255)]
+        [int]
+        $Red = 0,
+        
+        [ValidateRange(0,255)]
+        [int]
+        $Green = 0,
+
+        [ValidateRange(0,255)]
+        [int]
+        $Blue = 0,
+
+        [switch]
+        $Inner
+    )
+    
+    begin {
+        #creating pen object
+        $color = [System.Drawing.Color]::FromArgb($Alpha, $Red, $Green, $Blue)
+        $pen = [System.Drawing.Pen]::new($color, $LineWidth)
+        $pen.Alignment = [System.Drawing.Drawing2D.PenAlignment]::Inset
+        $imageExtensions = @(".gif", ".ico", ".jpeg", ".jpg", ".jpe", ".png")
+    }
+    
+    process {
+        foreach ($p in $Path) {
+            
+            $convertedPath = Convert-Path $p
+            Write-Verbose $convertedPath
+
+            $targetExtension = [System.IO.Path]::GetExtension($convertedPath)
+            if ($targetExtension.ToLower() -notin $imageExtensions) {
+                continue
+            }
+
+            #creating image object
+            
+            if ($Inner) {
+                $bitmap = [System.drawing.Bitmap]::new($convertedPath)
+                $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+            } else {
+                $bitmapBeforeExpanding = [System.drawing.Bitmap]::new($convertedPath)
+                $bitmap = [System.drawing.Bitmap]::new($bitmapBeforeExpanding.Width + $LineWidth * 2, $bitmapBeforeExpanding.Height + $LineWidth * 2)
+                $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+                $graphics.DrawImage($bitmapBeforeExpanding, $LineWidth, $LineWidth, $bitmapBeforeExpanding.Width, $bitmapBeforeExpanding.Height)
+            }
+
+            #creating rectangle object
+            $rectangle = [System.Drawing.Rectangle]::new(0, 0, $bitmap.Width - 1, $bitmap.Height - 1)
+
+            # drawing frame on image
+            $graphics.DrawRectangle($pen, $rectangle)
+            $graphics.Dispose()
+
+            # saving image
+            $baseName = [System.IO.Path]::GetFileNameWithoutExtension($convertedPath)
+            $newPath = $convertedPath -replace $baseName, "$($baseName)_A$($Alpha)R$($Red)G$($Green)B$($Blue)_$($LineWidth)px"
+
+            Write-Verbose $newPath
+
+            $bitmap.Save($newPath)
+            $bitmap.Dispose()
+
+            Get-Item -Path $newPath
+        }
+    }
+    
+    end {
+        $pen.Dispose()
+    }
+}
+
+Set-Alias -Name addfr -Value Add-FrameToImage
+
+Export-ModuleMember -Function * -Alias *
