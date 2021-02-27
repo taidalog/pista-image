2eaff1fcd92ae0c3b9bd4f373531351658533f0e
taidalog <72747279+taidalog@users.noreply.github.com>
2021-02-27 19:42:13 +0900

    change Save-ExeIcon Now the function can accept inputs from pipeline

--- Save-ExeIcon/Save-ExeIcon.psm1
+++ Save-ExeIcon/Save-ExeIcon.psm1
@@ -1,4 +1,6 @@
-﻿function Save-ExeIcon {
+﻿Add-Type -AssemblyName System.Drawing
+
+function Save-ExeIcon {
     <#
     .SYNOPSIS
     Saves the icon file associated to an executive file.
@@ -7,28 +9,28 @@
     Save-ExeIcon function saves the icon file associated to an executive file.
     
     .EXAMPLE
-    Save-ExeIcon -Path "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -SavePath "C:\Users\[username]\Pictures\" -SaveName "powershell.ico"
+    Save-ExeIcon -Path "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Destination "C:\Users\[username]\Pictures\" -SaveName "powershell.ico"
     
     .EXAMPLE
     Save-ExeIcon -Path "C:\Program Files\PowerShell\7\pwsh.exe" -SaveName "powershell7.png"
-    
-    An icon file will be saved in the current directory.
+    # An icon file will be saved in the current directory.
     
     .EXAMPLE
-    Save-ExeIcon -Path "C:\Program Files\PowerShell\7\pwsh.exe" -Destination "powershell6.aaa"
-    
-    An icon file will be saved in the current directory as "powershell6.ico", because the extention ".aaa" is not supported as a format for picture file format.
-    (.ico will be used instead of unknown extentions.)
+    Save-ExeIcon -Path "C:\Program Files\PowerShell\7\pwsh.exe" -SaveName "powershell6.text"
+    # An icon file will be saved in the current directory as "powershell6.ico".
+    # The extention ".text" is not a picture file format, so ".ico" will be used.
+    # (".ico" will be used instead of unknown extentions.)
     
     .PARAMETER Path
-    An absolute path to the executive file of which you want to get an associated icon.
+    A path to the executive file of which you want to get an associated icon.
+    An absolute path or a relative path.
     
-    .PARAMETER SavePath
-    A path for the icon file created by Save-ExeIcon function.
+    .PARAMETER Destination
+    A path to the directory in which the created icon file will be saved.
     An absolute path or a relative path.
     
     .PARAMETER SaveName
-    A name for the icon file created by Save-ExeIcon function.
+    A name for the icon file to be created.
 
     .INPUTS
     
@@ -39,19 +41,24 @@
     #>
     [CmdletBinding()]
     param (
-        [Parameter(
-            Mandatory=$true,
-            Position = 0,
-            ValueFromPipelineByPropertyName
-            )]
-        [String]
+        # Specifies a path to one or more locations.
+        [Parameter(Mandatory=$true,
+                   Position=0,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   HelpMessage="Path to one or more locations.")]
+        [Alias("PSPath")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({Test-Path $_ -PathType 'Leaf'})]
+        [string[]]
         $Path,
 
         [Parameter(
             Position = 1
             )]
+            [ValidateScript({Test-Path $_ -PathType 'Container'})]
         [String]
-        $SavePath = (Convert-Path .),
+        $Destination = (Convert-Path .),
 
         [Parameter(
             Position = 2
@@ -61,8 +68,6 @@
     )
     
     begin {
-        Add-Type -AssemblyName System.Drawing
-
         $imageFormatsAndExtensions = @{
             [System.Drawing.Imaging.ImageFormat]::Bmp = @(".bmp")
             [System.Drawing.Imaging.ImageFormat]::Gif = @(".gif")
@@ -73,39 +78,42 @@
     }
     
     process {
-        if (Split-Path $SavePath -IsAbsolute) {
-            [string]$absoluteSavePath = $SavePath
-        } else {
-            [string]$absoluteSavePath = Join-Path (Convert-Path .) $SavePath
-        }
+        foreach ($p in $Path) {
+            $convertedPath = Convert-Path $p
 
-        if ($SaveName -ne '') {
-            if ([System.IO.Path]::GetExtension($SaveName) -ne '') {
-                [string]$tmpSaveName = $SaveName
+            # creating save name
+            if ($SaveName -eq '') {
+                [string]$tmpSaveName = [System.IO.Path]::GetFileNameWithoutExtension($convertedPath) + '.ico'
             } else {
-                [string]$tmpSaveName = $SaveName + '.ico'
+                if ([System.IO.Path]::GetExtension($SaveName) -eq '') {
+                    [string]$tmpSaveName = $SaveName + '.ico'
+                } else {
+                    [string]$tmpSaveName = $SaveName
+                }
             }
-        } else {
-            [string]$tmpSaveName = [System.IO.Path]::GetFileNameWithoutExtension($Path) + '.ico'
-        }
-        
-        [string]$saveFullName = Join-Path ($absoluteSavePath, $tmpSaveName)
-        $targetExtension = [System.IO.Path]::GetExtension($tmpSaveName).ToLower()
-        
-        foreach ($key in $imageFormatsAndExtensions.Keys) {
-            if ($targetExtension -in $imageFormatsAndExtensions[$key]) {
-                $saveFormat = $key
-            } else {
-                $saveFormat = [System.Drawing.Imaging.ImageFormat]::Icon
+            
+            # creating save path
+            $absoluteDestination = Convert-Path $Destination
+            $saveFullName = Join-Path ($absoluteDestination, $tmpSaveName)
+            
+            # deciding extension to save with
+            $targetExtension = [System.IO.Path]::GetExtension($tmpSaveName).ToLower()
+            
+            foreach ($key in $imageFormatsAndExtensions.Keys) {
+                if ($targetExtension -in $imageFormatsAndExtensions[$key]) {
+                    $saveFormat = $key
+                } else {
+                    $saveFormat = [System.Drawing.Imaging.ImageFormat]::Icon
+                }
             }
-        }
 
-        $icon = [System.Drawing.Icon]::ExtractAssociatedIcon($Path)
-        $icon.ToBitmap().Save($saveFullName, $saveFormat)
+            $icon = [System.Drawing.Icon]::ExtractAssociatedIcon($convertedPath)
+            $icon.ToBitmap().Save($saveFullName, $saveFormat)
+            $icon.Dispose()
+        }
     }
     
     end {
-        $icon.Dispose()
     }
 }
 
