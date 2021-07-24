5a2027cd34a99d1e848ac3cf2afcb82b8819d96a
taidalog <72747279+taidalog@users.noreply.github.com>
2021-07-24 14:51:58 +0900

    add ParameterSet Now color can be spesified in ARGB or in Color struct.

--- Add-FrameToImage/Add-FrameToImage.psm1
+++ Add-FrameToImage/Add-FrameToImage.psm1
@@ -3,45 +3,113 @@ Set-StrictMode -Version Latest
 Add-Type -AssemblyName System.Drawing
 
 function Add-FrameToImage {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="ARGB")]
     param (
         # Specifies a path to one or more locations.
         [Parameter(Mandatory=$true,
                    Position=0,
-                   ParameterSetName="Path",
+                   ParameterSetName="ARGB",
                    ValueFromPipeline=$true,
                    ValueFromPipelineByPropertyName=$true,
-                   HelpMessage="Path to one or more locations.")]
+                   HelpMessage="Path to one or more locations."
+                   )]
+        [Parameter(Mandatory=$true,
+                   Position=0,
+                   ParameterSetName="Color",
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   HelpMessage="Path to one or more locations."
+                   )]
         [Alias("PSPath")]
         [ValidateNotNullOrEmpty()]
         [ValidateScript({Test-Path $_ -PathType 'Leaf'})]
         [string[]]
         $Path,
 
-        [ValidateRange(1,[int]::MaxValue)]
-        [int]
-        $LineWidth = 1,
-
+        [Parameter(Mandatory=$false,
+                   Position=1,
+                   ParameterSetName="ARGB",
+                   HelpMessage="ARGB value for Alpha."
+                   )]
+        [Alias()]
         [byte]
         $Alpha = 255,
         
+        [Parameter(Mandatory=$false,
+                   Position=2,
+                   ParameterSetName="ARGB",
+                   HelpMessage="ARGB value for Red."
+                   )]
+        [Alias()]
         [byte]
         $Red = 0,
         
+        [Parameter(Mandatory=$false,
+                   Position=3,
+                   ParameterSetName="ARGB",
+                   HelpMessage="ARGB value for Green."
+                   )]
+        [Alias()]
         [byte]
         $Green = 0,
 
+        [Parameter(Mandatory=$false,
+                   Position=4,
+                   ParameterSetName="ARGB",
+                   HelpMessage="ARGB value for Blue."
+                   )]
+        [Alias()]
         [byte]
         $Blue = 0,
 
+        [Parameter(Mandatory=$false,
+                   Position=1,
+                   ParameterSetName="Color",
+                   HelpMessage="Color struct or color name in <color> enums."
+                   )]
+        [Alias()]
+        [System.Drawing.Color]
+        $Color,
+
+        [Parameter(Mandatory=$false,
+                   Position=5,
+                   ParameterSetName="ARGB",
+                   HelpMessage="Width of line."
+                   )]
+        [Parameter(Mandatory=$false,
+                   Position=2,
+                   ParameterSetName="Color",
+                   HelpMessage="Width of line."
+                   )]
+        [Alias()]
+        [ValidateScript({$_ -gt 0})]
+        [int]
+        $LineWidth = 1,
+
+        [Parameter(Mandatory=$false,
+                   ParameterSetName="ARGB",
+                   HelpMessage="Indicates that the line will be drawn inside the picture."
+                   )]
+        [Parameter(Mandatory=$false,
+                   ParameterSetName="Color",
+                   HelpMessage="Indicates that the line will be drawn inside the picture."
+                   )]
         [switch]
         $Inner
     )
     
     begin {
         #creating pen object
-        $color = [System.Drawing.Color]::FromArgb($Alpha, $Red, $Green, $Blue)
-        $pen = [System.Drawing.Pen]::new($color, $LineWidth)
+        switch ($PSCmdlet.ParameterSetName) {
+            "ARGB" {
+                $innerColor = [System.Drawing.Color]::FromArgb($Alpha, $Red, $Green, $Blue)
+            }
+            "Color" {
+                $innerColor = $Color
+            }
+        }
+
+        $pen = [System.Drawing.Pen]::new($innerColor, $LineWidth)
         $pen.Alignment = [System.Drawing.Drawing2D.PenAlignment]::Inset
         $imageExtensions = @(".gif", ".ico", ".jpeg", ".jpg", ".jpe", ".png")
     }
